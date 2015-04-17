49cfb557a8de57a8c3ab941424eb6a39a0a8f3ad
rudyrahman@windowslive.com <Developer2015>
2015-04-17 11:16:36 +0700

    For the World.

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cboItemCode = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtItemDesc = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtQtyKg = New System.Windows.Forms.TextBox()
+        Me.txtFishTotal = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(517, 218)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(130, 31)
+        Me.btnSave.TabIndex = 0
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Item Code:"
+        '
+        'cboItemCode
+        '
+        Me.cboItemCode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboItemCode.FormattingEnabled = True
+        Me.cboItemCode.Location = New System.Drawing.Point(77, 5)
+        Me.cboItemCode.Name = "cboItemCode"
+        Me.cboItemCode.Size = New System.Drawing.Size(220, 21)
+        Me.cboItemCode.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Item Desc:"
+        '
+        'txtItemDesc
+        '
+        Me.txtItemDesc.Location = New System.Drawing.Point(77, 32)
+        Me.txtItemDesc.Name = "txtItemDesc"
+        Me.txtItemDesc.Size = New System.Drawing.Size(558, 20)
+        Me.txtItemDesc.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 61)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Qty Kg:"
+        '
+        'txtQtyKg
+        '
+        Me.txtQtyKg.Location = New System.Drawing.Point(77, 58)
+        Me.txtQtyKg.Name = "txtQtyKg"
+        Me.txtQtyKg.Size = New System.Drawing.Size(103, 20)
+        Me.txtQtyKg.TabIndex = 6
+        Me.txtQtyKg.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtFishTotal
+        '
+        Me.txtFishTotal.Location = New System.Drawing.Point(77, 84)
+        Me.txtFishTotal.Name = "txtFishTotal"
+        Me.txtFishTotal.Size = New System.Drawing.Size(103, 20)
+        Me.txtFishTotal.TabIndex = 7
+        Me.txtFishTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 87)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Fish Total:"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(381, 218)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(130, 31)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(659, 261)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtFishTotal)
+        Me.Controls.Add(Me.txtQtyKg)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtItemDesc)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cboItemCode)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSave)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Input Data Test"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cboItemCode As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtItemDesc As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtQtyKg As System.Windows.Forms.TextBox
+    Friend WithEvents txtFishTotal As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,58 @@
+﻿Imports ADODB
+
+Public Class Form1
+    Dim cn As New ADODB.Connection
+    Dim rs As New ADODB.Recordset
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            cn.ConnectionString = "Provider=SQLNCLI11;Server=192.168.0.1;Database=AN_SUMATRA;Uid=itdevelopment;Pwd=itdevelopment2015"
+            cn.Open()
+
+            rs = cn.Execute("SELECT [code] FROM [AN_SUMATRA].[dbo].[OT_t_test_01] ORDER BY [code] ASC")
+            If ((rs.EOF = False) And (rs.BOF = False)) = True Then
+                While Not rs.EOF
+                    cboItemCode.Items.Add(rs(0).Value.ToString)
+                    rs.MoveNext()
+                End While
+            End If
+
+            Me.Show()
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+
+    End Sub
+
+    Private Sub cboItemCode_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboItemCode.SelectedIndexChanged
+        Try
+            rs = cn.Execute("SELECT TOP 1 [description] FROM [AN_SUMATRA].[dbo].[OT_t_test_01] WHERE [code]='" & cboItemCode.Text.ToString & "' ORDER BY [code] ASC")
+            If (rs.EOF = False) And (rs.BOF = False) Then
+                txtItemDesc.Text = rs(0).Value.ToString
+            Else
+                txtItemDesc.Text = ""
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+        
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Try
+           
+
+            Dim sqlInsert As String = "INSERT INTO [AN_SUMATRA].[dbo].[OT_t_test_02] ([code],[description],[qtykg],[qtyfish],[size],[inserttime]) VALUES "
+            sqlInsert = sqlInsert & "('" & cboItemCode.Text.ToString & "'" & _
+                                    ",'" & txtItemDesc.Text & "'" & _
+                                    ",'" & txtQtyKg.Text & "'" & _
+                                    ",'" & txtFishTotal.Text & "'" & _
+                                    ",'" & Math.Round(((Val(txtQtyKg.Text) / Val(txtFishTotal.Text)) * 1000), 0) & "'" & _
+                                    ", " & "GETDATE()" & " " & ")"
+            cn.Execute(sqlInsert)
+
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+    End Sub
+End Class
